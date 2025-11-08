1bf04eafe274d4f95f4744015aeb43fb01fb35b6
Listwas <listwa.patrykkr@gmail.com>
2025-11-08 14:09:51 +0100

    feat: roulette_selection, one point crossover

--- main.py
+++ main.py
@@ -30,4 +30,32 @@ def fitness(individual):
 
     return total_value
 
-print(fitness(random_individual()))
+def roulette_selection(population, fitnesses):
+    total_fitness = sum(fitnesses)
+    if total_fitness == 0:
+        return random.choice(population)
+
+    pick = random.uniform(0, total_fitness)
+    current = 0
+
+    for individual, fit in zip(population, fitnesses):
+        current += fit
+        if current > pick:
+            return individual
+
+def one_point_crossover(parent1, parent2):
+    point = random.randint(1, NUMBER_OF_GENES - 1)
+
+    child1 = parent1[:point] + parent2[point:]
+    child2 = parent2[:point] + parent1[point:]
+    return child1, child2
+
+p1 = [1, 0, 1, 1, 0]
+p2 = [0, 1, 0, 0, 1]
+
+c1, c2 = one_point_crossover(p1, p2)
+print("p1:", p1)
+print("p2:", p2)
+print("c1:", c1)
+print("c2:", c2)
+
